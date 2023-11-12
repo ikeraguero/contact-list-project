d65a437ea0b765bfdab80202e7f2b5c408b8da5f
ikeraguero <ikerpires407@gmail.com>
2023-11-11 23:11:19 -0300

    First Commit - Add Requisites and Main Function

---
+++ contact_list.c
@@ -0,0 +1,27 @@
+// REQUISITOS
+
+// i) incluir um novo contato; 
+// ii) excluir um contato existente; 
+// iii) alterar um contato existente; 
+// iv) listar todos os contatos cadastrados; e 
+// v) localizar um contato.
+
+// STRUCT DO CONTATO
+
+// i) id
+// ii) nome
+// iii) telefone
+// iv) tipo de contato
+
+
+#include <stdio.h>
+
+int main() {
+        printf("\nAGENDA DE CONTATOS TELEFôNICOS\n");
+        printf("[1] - Adicionar um contato\n");
+        printf("[2] - Deletar um contato\n");
+        printf("[3] - Editar um contato\n");
+        printf("[4] - Listar todos os contatos\n");
+        printf("[5] - Localizar um contato\n");
+        printf("[6] - Sair\n");
+}
